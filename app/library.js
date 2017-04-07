c9a383c72b6e3c2584301ea4eea10cfcac529a17
wonderweis <emmanuelgerald05@gmail.com>
2017-04-07 18:14:01 +0100

    Use of assert syntax

--- app/library.js
+++ app/library.js
@@ -4,25 +4,28 @@ module.exports = {
 
  /* Find the average of three integers */
  computeAverage: function(num1, num2, num3) {
-   var average;
+   var average = (num1 + num2 + num3)/3;
    return average;
  },
  
  /* Find the factorial of an integer */
  computeFactorial: function(num) {
-   var factorial;
+   var factorial = 1;
+   for (var i = num; i>0; i--) {
+      factorial *= i;
+   }
    return factorial;
  },
 
  /* Convert a given Celcius temperature to Fahrenheit */
  convertTempCtoF: function(cTemp) {
-   var fTemp;
+   var fTemp = (9 * cTemp / 5) + 32;
    return fTemp;
  },
 
  /* Convert a given Fahrenheit temperature to Celcius */
  convertTempFtoC: function(fTemp) {
-   var cTemp;
+   var cTemp = 5 * (fTemp - 32) / 9;
    return cTemp;
  }
 
