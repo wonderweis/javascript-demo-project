acb1196eab8c22d8b01e042821a5ad5139e89563
wonderweis <emmanuelgerald05@gmail.com>
2017-04-07 17:15:43 +0100

    Use of expect syntax

--- tests/tests.js
+++ tests/tests.js
@@ -3,34 +3,34 @@
 'use strict'
 
 var chai = require('chai');
-chai.should(); /*chai.should(), chai.expect, chai.assert*/
+var expect = chai.expect; /*chai.should(), chai.expect, chai.assert*/
 
 var myApp = require('../app/library.js');
 
 describe("Arithmetic Functionality", function() {
   it("should return 2 as average for 1, 2, 3", function() {
-    myApp.computeAverage(1,2,3).should.equal(2);
+    expect(myApp.computeAverage(1,2,3)).to.equal(2);
   })
   it("should return 5 as average for 3, 7, 5", function() {
-    myApp.computeAverage(3, 7, 5).should.equal(5);
+    expect(myApp.computeAverage(3, 7, 5)).to.equal(5);
   })
   it("should return 120 as factorial for 5", function() {
-    myApp.computeFactorial(5).should.equal(120);
+    expect(myApp.computeFactorial(5)).to.equal(120);
   })
 })
 
 
 describe("Temperature Conversion Functionality", function() {
   it("should return 104 for Celcius value 40", function() {
-    myApp.convertTempCtoF(40).should.equal(104);
+    expect(myApp.convertTempCtoF(40)).to.equal(104);
   })
   it("should return 140 for Celcius value 60", function() {
-    myApp.convertTempCtoF(60).should.equal(140);
+    expect(myApp.convertTempCtoF(60)).to.equal(140);
   })
   it("should return 60 for Fahrenheit value 140", function() {
-    myApp.convertTempFtoC(140).should.equal(60);
+    expect(myApp.convertTempFtoC(140)).to.equal(60);
   })
   it("should return 40 for Fahrenheit value 104", function() {
-    myApp.convertTempFtoC(104).should.equal(40);
+    expect(myApp.convertTempFtoC(104)).to.equal(40);
   })
 })
